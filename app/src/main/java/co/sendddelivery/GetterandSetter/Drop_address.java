ab5322c398acccdf647bc5fea44b51d9b05db097
harsh karanpuria <harshkaranpuria@gmail.com>
2015-08-06 12:42:38 +0530

    trial

---
+++ app/src/main/java/co/sendddelivery/GetterandSetter/Drop_address.java
@@ -0,0 +1,71 @@
+package co.sendddelivery.GetterandSetter;
+
+/**
+ * Created by harshkaranpuria on 8/6/15.
+ */
+public class Drop_address {
+    private String drop_address_pk;
+    private String drop_address_city;
+    private String drop_address_country;
+    private String drop_address_flat_no;
+    private String drop_address_locality;
+    private String drop_address_pincode;
+    private String drop_address_state;
+
+    public String getDrop_address_state() {
+        return drop_address_state;
+    }
+
+    public void setDrop_address_state(String drop_address_state) {
+        this.drop_address_state = drop_address_state;
+    }
+
+    public String getDrop_address_pincode() {
+        return drop_address_pincode;
+    }
+
+    public void setDrop_address_pincode(String drop_address_pincode) {
+        this.drop_address_pincode = drop_address_pincode;
+    }
+
+    public String getDrop_address_locality() {
+        return drop_address_locality;
+    }
+
+    public void setDrop_address_locality(String drop_address_locality) {
+        this.drop_address_locality = drop_address_locality;
+    }
+
+    public String getDrop_address_flat_no() {
+        return drop_address_flat_no;
+    }
+
+    public void setDrop_address_flat_no(String drop_address_flat_no) {
+        this.drop_address_flat_no = drop_address_flat_no;
+    }
+
+    public String getDrop_address_country() {
+        return drop_address_country;
+    }
+
+    public void setDrop_address_country(String drop_address_country) {
+        this.drop_address_country = drop_address_country;
+    }
+
+    public String getDrop_address_city() {
+        return drop_address_city;
+    }
+
+    public void setDrop_address_city(String drop_address_city) {
+        this.drop_address_city = drop_address_city;
+    }
+
+    public String getDrop_address_pk() {
+        return drop_address_pk;
+    }
+
+    public void setDrop_address_pk(String drop_address_pk) {
+        this.drop_address_pk = drop_address_pk;
+    }
+
+}
